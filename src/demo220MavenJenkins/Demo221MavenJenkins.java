24119cb6a0d3abd3296fce3e4b6e159463de6472
twrandunuge <thilanka77.wijayani@gmail.com>
2020-02-26 12:53:03 -0800

    My Initial Commit

---
+++ src/demo220MavenJenkins/Demo221MavenJenkins.java
@@ -0,0 +1,18 @@
+package demo220MavenJenkins;
+
+public class Demo221MavenJenkins {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("My Maven Jenkins Project");
+		myFirstMavenJenkinProject();
+	
+	}
+	
+	public static void myFirstMavenJenkinProject() {
+		
+		System.out.println("My First Maven Jenkins Project");
+	}
+
+}
